ee2e76aefbd1e489be1876dabfb0264df63fc54b
Aris Bezas <igoumeninja@gmail.com>
2015-03-20 10:19:23 +0000

    Stem Project saved on: Fri Mar 20 10:19:23 GMT 2015

--- src/testApp.h
+++ src/testApp.h
@@ -18,7 +18,7 @@ public:
   void draw();
   
   void keyPressed  (int key);
-  void keyReleased(int key);
+  void keyReleased(int key);
   void mouseMoved(int x, int y );
   void mouseDragged(int x, int y, int button);
   void mousePressed(int x, int y, int button);
